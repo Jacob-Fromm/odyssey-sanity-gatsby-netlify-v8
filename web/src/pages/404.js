1ed8edc9e2198e73edc39f09ee0751c067ca5a7f
jakeFromm <jacobfromm@gmail.com>
2021-03-07 18:41:43 -0500

    more changes to 404 page

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -6,7 +6,7 @@ import SEO from '../components/seo'
 const NotFoundPage = () => (
   <Layout>
     <SEO title='404: Not found' />
-    <h1>NOT FOUND</h1>
+    <h1>PAGE NOT FOUND</h1>
     <p>This page does not exist. For your own safety, please return from whence you came.</p>
   </Layout>
 )
